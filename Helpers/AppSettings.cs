8cc5d008f6853659cacd6861ff96e125c6c55920
EricksonVaz <ericksoncv1@outlook.com>
2022-10-16 19:42:33 -0100

    first commit

---
+++ Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthJWTAPI.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+using AuthJWTAPI.Helpers;
+using AuthJWTAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+builder.Services.AddScoped<IUserService, UserService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+// custom jwt auth middleware
+app.UseMiddleware<JwtMiddleware>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
